2534a40ed6a44174f48232d5189faebd0fddac37
soo-jin-lim <tn7969@naver.com>
2023-05-10 14:29:18 +0900

    .

---
+++ Tunnel/cyTunnel01_MonitoringRoom/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace cyTunnel01_MonitoringRoom
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnsolution_Click(object sender, RoutedEventArgs e)
+        {
+
+           
+        }
+
+        private void Btnwhat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGenDBControl_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
